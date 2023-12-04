d61df04b3d5b7625ee7ceda976e4bb82cfb27af2
el-bekasto <beksei7@gmail.com>
2023-12-04 23:10:27 +0600

    added readme and swagger and requirements.txt

--- drf_test_project/settings.py
+++ drf_test_project/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'users.apps.UsersConfig',
     'book_catalog.apps.BookCatalogConfig',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework_swagger',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- drf_test_project/urls.py
+++ drf_test_project/urls.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='',
+        default_version='',
+        description='',
+        contact=openapi.Contact(email='beksei7@gmail.com')
+    ),
+    public=True,
+    permission_classes=[AllowAny]
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('users/', include('users.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
